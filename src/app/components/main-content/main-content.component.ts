943d1f2852b176a0279b787d53899695ade97aaa
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-20 21:29:37 -0500

    feat: create banner section component

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AccordionSectionComponent } from '../accordion-section/accordion-section.component';
+import { BannerSectionComponent } from '../banner-section/banner-section.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AccordionSectionComponent],
+  imports: [AccordionSectionComponent, BannerSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
