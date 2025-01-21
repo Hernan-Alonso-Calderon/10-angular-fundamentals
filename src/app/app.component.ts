f1164ebd6b177824b6aa9128bd1618f40f83bbc6
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-20 20:56:19 -0500

    feat: create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { AcordionComponent } from './components/acordion/acordion.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, AcordionComponent],
+  imports: [FooterComponent, AcordionComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
