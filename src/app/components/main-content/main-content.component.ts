6c8cf4b75a425a65fae15078e35fc50cd06ff5fa
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-20 21:40:34 -0500

    feat: create article section component

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { AccordionSectionComponent } from '../accordion-section/accordion-section.component';
 import { BannerSectionComponent } from '../banner-section/banner-section.component';
+import { ArticleSectionComponent } from '../article-section/article-section.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AccordionSectionComponent, BannerSectionComponent],
+  imports: [
+    AccordionSectionComponent,
+    BannerSectionComponent,
+    ArticleSectionComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
