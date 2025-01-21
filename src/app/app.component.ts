b4816e49be102ea6e590247311467af01527fbca
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-20 21:13:24 -0500

    feat: create main content component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
-import { AcordionComponent } from './components/acordion/acordion.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, AcordionComponent, HeaderComponent],
+  imports: [FooterComponent, MainContentComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { AcordionComponent } from '../acordion/acordion.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [AcordionComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
